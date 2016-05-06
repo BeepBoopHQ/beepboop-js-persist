81e917067dd8b92765588dd365b73cd0e806ac7c
Brad Harris <bmharris@gmail.com>
2016-05-06 09:09:07 -0600

    add debug logging - return single interface wrapping provider instead of provider directly

--- index.js
+++ index.js
@@ -1,18 +1,86 @@
 var deap = require('deap')
 var serviceProvider = require('./lib/service-provider')
 var memoryProvider = require('./lib/memory-provider')
+var Logger = require('./lib/logger')
 
 module.exports = function NewKV (options) {
   var config = deap.update({
+    debug: false,
     serialize: true,
     token: process.env.BEEPBOOP_TOKEN,
     url: 'http://persist'
   }, options || {})
 
-  if (!config.token) {
-    console.log('USING PERSIST IN MEMORY PROVIDER')
-    return memoryProvider(config)
-  }
+  var logger = config.logger || Logger(config.debug)
+  var provider = !config.token ? memoryProvider(config) : serviceProvider(config)
 
-  return serviceProvider(config)
+  // return a wrapper around provider to normalize args and logging
+  return {
+    type: provider.type,
+
+    get: function (key, cb) {
+      provider.get(key, function (err, value) {
+        if (err) {
+          logger.error('Error calling get(%s): %s', key, err.message)
+        } else {
+          logger.debug('get(%s)', key)
+        }
+
+        cb(err, value)
+      })
+    },
+    mget: function (keys, cb) {
+      provider.mget(keys, function (err, value) {
+        if (err) {
+          logger.error('Error calling mget([%s]): %s', keys.join(','), err.message)
+        } else {
+          logger.debug('mget([%s])', keys.join(','))
+        }
+
+        cb(err, value)
+      })
+    },
+    set: function (key, value, cb) {
+      cb = cb || noop
+
+      provider.set(key, value, function (err, value) {
+        if (err) {
+          logger.error('Error calling set(%s): %s', key, err.message)
+        } else {
+          logger.debug('set(%s)', key)
+        }
+
+        cb(err, value)
+      })
+    },
+    del: function (key, cb) {
+      provider.del(key, function (err, value) {
+        if (err) {
+          logger.error('Error calling del(%s): %s', key, err.message)
+        } else {
+          logger.debug('del(%s)', key)
+        }
+
+        cb(err, value)
+      })
+    },
+    list: function (before, cb) {
+      if (typeof before === 'function') {
+        cb = before
+        before = null
+      }
+
+      provider.list(before, function (err, value) {
+        if (err) {
+          logger.error('Error calling list(): %s', err.message)
+        } else {
+          logger.debug('list()')
+        }
+
+        cb(err, value)
+      })
+    }
+  }
 }
+
+function noop () {}
